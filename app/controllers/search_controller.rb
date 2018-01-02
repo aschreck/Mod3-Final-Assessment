feed713aa529b6a49f8031c2eb9a290ab107fcc8
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-02 11:05:29 -0700

    Built rudamentary view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ActionController::Base
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params["search"]},25))?format=json&show=storeType,city,longName,distance,phone&apiKey=#{ENV["best_buy_key"]}")
     stores = JSON.parse(response.body)
     require 'pry'; binding.pry
-      
+    @
   end 
 
 end 
