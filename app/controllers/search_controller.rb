d758aa7428558134f047606a4656ac51404ae048
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-02 11:14:10 -0700

    wrote store class and added it to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,13 @@ class SearchController < ActionController::Base
 
   def index
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params["search"]},25))?format=json&show=storeType,city,longName,distance,phone&apiKey=#{ENV["best_buy_key"]}")
-    stores = JSON.parse(response.body)
-    require 'pry'; binding.pry
-    @
+    json_stores = JSON.parse(response.body)
+    @stores = []
+    
+    json_stores.each do |json_store|
+      Store.new(json_store)
+      require 'pry'; binding.pry
+    end
   end 
 
 end 
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,1 +1,11 @@
-class
+class Store 
+  def initialize(info)
+    require 'pry'; binding.pry
+    
+    @name = info["longName"] 
+    @city = info["city"]
+    @type = info["storeType"] 
+    @phone = info["phone"] 
+    @distance = info["distance"] 
+  end
+end 
